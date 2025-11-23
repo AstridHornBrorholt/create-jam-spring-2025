cc66d3dffa00fb86ae7f0ba1e9b35de5f2b98b40
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2025-11-23 11:55:18 +0100

    Fix crash when destroying "next" block when it's also "held"

--- Scripts/destroy.gd
+++ Scripts/destroy.gd
@@ -104,6 +104,8 @@ func _on_select_held_mouse_entered() -> void:
 	select_held_button.grab_focus()
 
 func _on_select_next_button_down() -> void:
+	if next_tetriminos.template.equals(held_tetriminos.template):
+		CurrentRun.previously_held = TetriminosTemplate.new([])
 	destroy(next_tetriminos)
 
 
