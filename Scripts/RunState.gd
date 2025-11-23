cef745d5b202680c87338b6f2f628b5a04c85398
Asger Horn Brorholt <asgerhb@cs.aau.dk>
2025-11-23 15:52:43 +0100

    Fix first level score being the same as the level where the previous run ended

--- Scripts/RunState.gd
+++ Scripts/RunState.gd
@@ -77,6 +77,9 @@ func _init() -> void:
 	reset()
 
 func reset():
+	next_reward = LevelOption.RewardType.Create
+	next_score_goal = get_level()[0]
+	next_time_limit = get_level()[1]
 	# var tetrimino_generator = TetriminoGenerator.new()
 	stash = [ L, J, T, I, S, Z, O ]
 	level = 0
